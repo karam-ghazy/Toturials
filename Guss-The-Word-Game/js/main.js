7ee1d793f71c6fe16ae5608e6cc5a9e69a02e8cc
karam-ghazy <146335603+karam-ghazy@users.noreply.github.com>
2024-07-24 16:18:49 +0300

    finsh create the project with some edit by karam

--- Guss-The-Word-Game/js/main.js
+++ Guss-The-Word-Game/js/main.js
@@ -11,6 +11,9 @@ document.querySelector(
 let triesNumber = 6;
 let lettersNumber = 6;
 let currentTry = 1;
+let hintsNumber = 2;
+// to know if we win and all letter in fields
+let count;
 
 //guss word and array of words chosen
 let gussWord = "";
@@ -25,6 +28,10 @@ let words = [
   "school",
 ];
 gussWord = words[Math.floor(Math.random() * words.length)];
+let messageArea = document.querySelector(".message");
+
+let spanHint = document.querySelector(".hint span");
+spanHint.innerHTML = hintsNumber;
 
 function generateInputs() {
   let inputContainer = document.querySelector(".inputs");
@@ -39,7 +46,7 @@ function generateInputs() {
       let input = document.createElement("input");
       input.type = "text";
       input.setAttribute("maxlength", 1);
-      input.id = `guss-${1}-letter${j}`;
+      input.id = `guss-${i}-letter${j}`;
       Maindiv.appendChild(input);
     }
     inputContainer.appendChild(Maindiv);
@@ -87,13 +94,21 @@ function RightLeftMove(inputs) {
   });
 }
 
-let check = document.querySelector(".check");
-check.addEventListener("click", handleCheck);
+let checkBtn = document.querySelector(".check");
+checkBtn.addEventListener("click", handleCheck);
 
 console.log(gussWord);
 
 function handleCheck() {
-  let success = true;
+  let success = checker();
+  console.log(gussWord);
+  //check if user win or not then check if next checker is avialable
+  nextChecker(success);
+}
+
+function checker() {
+  let success = null;
+  count = 0;
   for (let i = 1; i <= lettersNumber; i++) {
     //get the current input and the actual letter in each case
     let input = document.querySelector(`#guss-${currentTry}-letter${i}`);
@@ -104,7 +119,10 @@ function handleCheck() {
 
     if (letter == actuallLetter) {
       //if letter correct and in the right place
+      count++;
+      // console.log(count);
       input.classList.add("yes-in-place");
+      success = true;
     } else if (gussWord.includes(letter) && letter != "") {
       //if letter correct and not in the right place
       input.classList.add("not-in-place");
@@ -114,6 +132,83 @@ function handleCheck() {
       input.classList.add("no");
       success = false;
     }
+  }
+  return success;
+}
+
+function nextChecker(success) {
+  //check if user is win or not
+  if (success && count == lettersNumber) {
+    messageArea.innerHTML = `you win the word guss is <span>${gussWord}</span>`;
+    checkBtn.disabled = true;
+    hintBtn.disabled = true;
+    let divInputs = document.querySelectorAll(".inputs > div");
+    divInputs.forEach((input) => input.classList.add("disabled-input"));
+  } else {
+    //add disabled to current input try
+    document
+      .querySelector(`.try-${currentTry}`)
+      .classList.add("disabled-input");
+    let currenInputs = document.querySelectorAll(`.try-${currentTry} input`);
+    currenInputs.forEach((input) => (input.disabled = true));
+
+    //remove disabled from next input try
+    let el = document.querySelector(`.try-${currentTry + 1} `);
+    if (el) {
+      currentTry++;
+      document
+        .querySelector(`.try-${currentTry}`)
+        .classList.remove("disabled-input");
+      let nextInputs = document.querySelectorAll(`.try-${currentTry} input`);
+      nextInputs.forEach((input) => (input.disabled = false));
+      el.children[1].focus();
+    } else {
+      checkBtn.disabled = true;
+      hintBtn.disabled = true;
+      messageArea.innerHTML = `you loss the word guss is <span>${gussWord}</span>`;
+    }
+  }
+}
+
+let hintBtn = document.querySelector(".hint");
+hintBtn.onclick = getHint;
+
+function getHint() {
+  if (hintsNumber > 0) {
+    let currentInputFields = document.querySelectorAll(
+      `.try-${currentTry} input`
+    );
+    let emptyInputFields = Array.from(currentInputFields).filter(
+      (input) => input.value == ""
+    );
+    hintsNumber--;
+    spanHint.innerHTML = hintsNumber;
+    if (emptyInputFields.length > 0) {
+      let randomInedx = Math.floor(Math.random() * emptyInputFields.length);
+      let randomInput = emptyInputFields[randomInedx];
+      let indexToFill = Array.from(currentInputFields).indexOf(randomInput);
+      // console.log(randomInedx);
+      // console.log(randomInput);
+      // console.log(indexToFill);
+      randomInput.value = gussWord[indexToFill].toUpperCase();
+    }
+  }
+  if (hintsNumber === 0) {
+    hintBtn.disabled = true;
+  }
+}
+addEventListener("keydown", handleBackSpace);
+function handleBackSpace(event) {
+  if (event.key == "Backspace") {
+    let inputs = document.querySelectorAll(`.try-${currentTry} input`);
+    let currentIndex = Array.from(inputs).indexOf(document.activeElement);
+    let prevIndex = currentIndex - 1;
+
+    let currenInput = inputs[currentIndex];
+    let prevInput = inputs[prevIndex];
+    currenInput.value = "";
+    prevInput.value = "";
+    prevInput.focus();
   }
 }
 
